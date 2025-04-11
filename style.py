e0c74bde71c66a6caab04250bfdd8f21d07a52f8
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-11 22:02:15 +0300

    Update style.py

--- style.py
+++ style.py
@@ -1,15 +1,1 @@
-import streamlit as st
 
-def apply_custom_style():
-    st.markdown("""
-        <style>
-        .stApp {
-            background-color: #f8f9fa;
-        }
-        .stButton>button {
-            color: white;
-            background-color: #4CAF50;
-            border-radius: 8px;
-        }
-        </style>
-    """, unsafe_allow_html=True)
