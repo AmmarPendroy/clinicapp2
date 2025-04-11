54cafba55e829cb4c5a6442cbc538e4af68e42c7
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-11 22:02:27 +0300

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,9 +1,1 @@
-import streamlit as st
 
-def sidebar_navigation():
-    st.sidebar.title("📋 Menu")
-    return st.sidebar.radio("Navigate to", [
-        "📊 Client Overview", 
-        "➕ Add Client", 
-        "📞 Contact Info"
-    ])
