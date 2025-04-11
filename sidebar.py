cdf847eacf5dbdf7789e0920179a8461f20949cc
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-11 21:59:22 +0300

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 def sidebar_navigation():
     st.sidebar.title("📋 Menu")
-    return st.sidebar.radio("Go to", ["📊 Client Overview", "➕ Add Client", "📞 Contact Info"])
+    return st.sidebar.radio("Navigate to", [
+        "📊 Client Overview", 
+        "➕ Add Client", 
+        "📞 Contact Info"
+    ])
